853d67f9aa663dd2811a78904a9fe8badc0c1719
naimulabeer <naimul.h.abeer@gmail.com>
2023-08-26 04:20:58 +0600

    directory component added

---
+++ src/components/category-item/CategoryItem.jsx
@@ -0,0 +1,19 @@
+import "./categories.styles.scss";
+
+function CategoryItem({ category }) {
+  const { imageUrl, title } = category;
+  return (
+    <div className="category-container">
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
+      <div className="category-body-container">
+        <h1>{title}</h1>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+}
+
+export default CategoryItem;
