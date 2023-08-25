5041e755d7c13bfb9c232185e047976e988bc963
naimulabeer <naimul.h.abeer@gmail.com>
2023-08-26 03:43:19 +0600

    setting up categories with map

--- src/App.js
+++ src/App.js
@@ -1,41 +1,36 @@
 function App() {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 2,
+      title: "Jackets",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+    },
+    {
+      id: 4,
+      title: "Mens",
+    },
+    {
+      id: 5,
+      title: "Womens",
+    },
+  ];
   return (
-    <div className="App">
-      <div className="category-container">
-        {/* <img></img> */}
-        <div className="category-body-container">
-          <h1>Hats</h1>
-          <p>Shop Now</p>
+    <div className="categories-container">
+      {categories.map(({ title }) => (
+        <div className="category-container">
+          <div className="category-body-container">
+            <h1>{title}</h1>
+            <p>Shop Now</p>
+          </div>
         </div>
-      </div>
-      <div className="category-container">
-        {/* <img></img> */}
-        <div className="category-body-container">
-          <h1>Jackets</h1>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img></img> */}
-        <div className="category-body-container">
-          <h1>Sneakers</h1>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img></img> */}
-        <div className="category-body-container">
-          <h1>Mens</h1>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img></img> */}
-        <div className="category-body-container">
-          <h1>Womens</h1>
-          <p>Shop Now</p>
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
