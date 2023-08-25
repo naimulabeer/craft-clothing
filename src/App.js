b928ab04a5a8fcda8ef3c2f08395fe724f22ab17
naimulabeer <naimul.h.abeer@gmail.com>
2023-08-26 03:18:07 +0600

    setting up clothing website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="category-container">
+        {/* <img></img> */}
+        <div className="category-body-container">
+          <h1>Hats</h1>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img></img> */}
+        <div className="category-body-container">
+          <h1>Jackets</h1>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img></img> */}
+        <div className="category-body-container">
+          <h1>Sneakers</h1>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img></img> */}
+        <div className="category-body-container">
+          <h1>Mens</h1>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {/* <img></img> */}
+        <div className="category-body-container">
+          <h1>Womens</h1>
+          <p>Shop Now</p>
+        </div>
+      </div>
     </div>
   );
 }
