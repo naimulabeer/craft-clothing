1a9fd7ba9ebd0b489ea280f5ff744f201a13aa45
naimulabeer <naimul.h.abeer@gmail.com>
2023-08-30 03:53:18 +0600

    added json data

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./routes/Home/Home";
 import Navigation from "./routes/Navigation/Navigation";
 import Authentication from "./routes/Authentication/Authentication";
-
-const Shop = () => {
-  return <h1>I am the shop page</h1>;
-};
+import Shop from "./routes/Shop/Shop";
 
 function App() {
   return (
