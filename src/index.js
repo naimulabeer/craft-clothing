83b4e60bdc378c2298160fe871ceeb6674dcc171
naimulabeer <naimul.h.abeer@gmail.com>
2023-08-30 06:09:35 +0600

    creating product and shop component

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import { UserProvider } from "./contexts/user-context";
+import { ProductsProvider } from "./contexts/product-context";
 
 import "./index.scss";
 
@@ -12,7 +13,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
--- src/routes/Shop/Shop.jsx
+++ src/routes/Shop/Shop.jsx
@@ -1,12 +1,16 @@
-import SHOP_DATA from "../../shop-data.json";
+import { useContext } from "react";
+
+import "./shop.styles.scss";
+
+import { ProductsContext } from "../../contexts/product-context";
+import ProductCard from "../../components/product-card/ProductCard";
 
 function Shop() {
+  const { products } = useContext(ProductsContext);
   return (
-    <div>
-      {SHOP_DATA.map(({ id, name }) => (
-        <div key={id}>
-          <h1>{name}</h1>
-        </div>
+    <div className="shop-container">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
       ))}
     </div>
   );
