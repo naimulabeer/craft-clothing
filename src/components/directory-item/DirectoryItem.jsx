c19cd82a372acf9081893c7b41e85fa964728752
naimulabeer <naimul.h.abeer@gmail.com>
2023-09-14 03:26:36 +0600

    Directory route created for homepage categories

--- src/components/directory-item/DirectoryItem.jsx
+++ src/components/directory-item/DirectoryItem.jsx
@@ -1,9 +1,15 @@
+import { useNavigate } from "react-router-dom";
 import "./directory-item.styles.scss";
 
 function DirectoryItem({ category }) {
-  const { imageUrl, title } = category;
+  const { imageUrl, title, route } = category;
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => {
+    navigate(route);
+  };
   return (
-    <div className="directory-item-container">
+    <div className="directory-item-container" onClick={onNavigateHandler}>
       <div
         className="background-image"
         style={{ backgroundImage: `url(${imageUrl})` }}
