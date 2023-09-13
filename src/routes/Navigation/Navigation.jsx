7e9d01b959822366c138a89d94854c7666c297f5
naimulabeer <naimul.h.abeer@gmail.com>
2023-09-13 06:28:58 +0600

    styled components with navigation bar

--- src/routes/Navigation/Navigation.jsx
+++ src/routes/Navigation/Navigation.jsx
@@ -11,7 +11,13 @@ import { CartContext } from "../../contexts/cart-context";
 
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
-import "./navigation.styles.scss";
+import {
+  NavigationContainer,
+  LogoContainer,
+  NavLinks,
+  NavLink,
+  LogoTitle,
+} from "./navigation.styles";
 
 function Navigation() {
   const { currentUser } = useContext(UserContext);
@@ -37,28 +43,23 @@ function Navigation() {
 
   return (
     <>
-      <div className="navigation">
-        <Link className="logo-container" to="/">
+      <NavigationContainer>
+        <LogoContainer to="/">
           <CraftLogo className="logo" />
-        </Link>
+          <LogoTitle>Craft Clothing</LogoTitle>
+        </LogoContainer>
 
-        <div className="nav-links-container">
-          <Link className="nav-link" to="/shop">
-            SHOP
-          </Link>
+        <NavLinks>
+          <NavLink to="/shop">SHOP</NavLink>
           {currentUser ? (
-            <span className="nav-link" onClick={signOutUser}>
-              SIGN OUT
-            </span>
+            <NavLink onClick={signOutUser}>SIGN OUT</NavLink>
           ) : (
-            <Link className="nav-link" to="/auth">
-              SIGN IN
-            </Link>
+            <NavLink to="/auth">SIGN IN</NavLink>
           )}
           <CartIcon />
-        </div>
+        </NavLinks>
         {isCartOpen && <CartDropdown />}
-      </div>
+      </NavigationContainer>
       <Outlet />
     </>
   );
---
+++ src/routes/Navigation/navigation.styles.jsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const NavigationContainer = styled.div`
+  height: 70px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 25px;
+  background-color: #6d597a;
+  border-radius: 16px;
+  padding: 4px;
+`;
+
+export const LogoContainer = styled(Link)`
+  padding: 20px;
+  display: flex;
+  gap: 5px;
+  align-items: center;
+`;
+
+export const LogoTitle = styled.h1`
+  font-size: 20px;
+  color: white;
+`;
+
+export const NavLinks = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  color: white;
+`;
+
+export const NavLink = styled(Link)`
+  padding: 10px 15px;
+  cursor: pointer;
+  color: white;
+  &:hover {
+    box-shadow: rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset,
+      rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset,
+      rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset,
+      rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px,
+      rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px,
+      rgba(0, 0, 0, 0.09) 0px 32px 16px;
+  }
+`;
