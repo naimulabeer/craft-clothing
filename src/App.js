6e5af622e7b8cf18a75e83d17153f5493f47bbed
naimulabeer <naimul.h.abeer@gmail.com>
2023-09-18 02:31:40 +0600

    implemented react toastify on adding a product

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import Navigation from "./routes/Navigation/Navigation";
 import Authentication from "./routes/Authentication/Authentication";
 import Shop from "./routes/Shop/Shop";
 import Checkout from "./routes/Checkout/Checkout";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="shop/*" element={<Shop />} />
-        <Route path="auth" element={<Authentication />} />
-        <Route path="checkout" element={<Checkout />} />
-      </Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="shop/*" element={<Shop />} />
+          <Route path="auth" element={<Authentication />} />
+          <Route path="checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+      <ToastContainer />
+    </>
   );
 }
 
--- src/components/product-card/ProductCard.jsx
+++ src/components/product-card/ProductCard.jsx
@@ -4,12 +4,19 @@ import { CartContext } from "../../contexts/cart-context";
 import "./product-card.styles.scss";
 
 import Button, { BUTTON_TYPE_CLASSES } from "../button/Button";
+import { toast } from "react-toastify";
 
 function ProductCard({ product }) {
   const { name, price, imageUrl } = product;
   const { addItemToCart } = useContext(CartContext);
 
-  const addProductToCart = () => addItemToCart(product);
+  const addProductToCart = () => {
+    addItemToCart(product);
+    toast.success(`Added ${product.name} to the cart`, {
+      position: "top-center",
+      autoClose: 2000,
+    });
+  };
 
   return (
     <div className="product-card-container">
